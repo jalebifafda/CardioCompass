06ef80c735db97c8c31d7dc6a5f0bc9d8047cb50
delkik <danielelkik@hotmail.com>
2024-02-10 20:46:32 -0500

    results message

--- src/Results.js
+++ src/Results.js
@@ -1,7 +1,10 @@
 const Results = () => {
     return (
         <div>
-            
+            <h1>!! Results !!</h1>
+            <p>You are at a high risk of having a heart disease! We highly recommend seeing a 
+                doctor for more testing.
+            </p>
         </div>
     )
 }
